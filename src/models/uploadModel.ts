3c526521c2229572114d2d11a449ecf2fa3ba40d
kobiowuquadri <kobiowuq@gmail.com>
2024-01-18 17:52:47 -0600

    set timestamps to true on uploadmodel

--- src/models/uploadModel.ts
+++ src/models/uploadModel.ts
@@ -4,9 +4,9 @@ import mongoose from "mongoose";
 const uploadSchema = new mongoose.Schema({
     path:{
         type:String,
-        required:[true, "Please upload an image"]
+        required:true
     }
-})
+}, {timestamps: true})
 
 
 const uploadModel = mongoose.model('uploadmodels', uploadSchema)
